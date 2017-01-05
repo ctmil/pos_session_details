e5de589ef2a4e8f325f6ae5bf73ef9683571df85
morrillo <gustavo.orrillo@moldeo.coop>
2017-01-05 15:48:29 -0300

    First commit

---
+++ __openerp__.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': "pos_session_details",
+    "version": "8.0.0.0.0",
+
+    'summary': """
+       POS Report session detailed""",
+
+    'description': """
+        POS Report session detailed
+    """,
+
+    'author': "Javier Marquez",
+    'website': "http://www.moldeointeractive.com.ar/es/",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Point_of_sale',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'base',
+        'point_of_sale',],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'report/pos_session_detail.xml',
+        'report/pos_small_report.xml',
+        'report/pos_order_details.xml',
+    ],
+    # only loaded in demonstration mode
+    # 'demo': [
+    #     'demo.xml',
+    # ],
+}
